a4082aa9145a728e56beeab79c516212000f5761
Sanjay <sanjays2402@gmail.com>
2020-07-03 23:51:43 +0530

    Update July 3 Solution.cpp

--- July 3/July 3 Solution.cpp
+++ July 3/July 3 Solution.cpp
@@ -7,12 +7,12 @@ public:
         if(N==0)
             N=14;
         
-        for(int i=0; i<N; i++)
+        while(N--)
         {
             temp = cells;
             for(int j=1; j<7; j++)
             {
-                cells[i] = (temp[j-1]==temp[j+1]);
+                cells[j] = (temp[j-1]==temp[j+1]);
             }
             cells[0]= 0;
             cells[7]= 0;
