c0064c1082f0dbc112c9f31958598c292a16e1ed
Sanjay <sanjays2402@gmail.com>
2020-07-31 17:40:53 +0530

    Create July 31 Solution.cpp

---
+++ July 31/July 31 Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        int a=0, b=1, c;
+        while(n-- > 0)
+        {
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        
+        return c;
+        
+    }
+};
